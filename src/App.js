edc3bc5f99d0c4b91de8d6da707de931723267cf
kavinda-jayasinghe <kavindajayasinghequn@gmail.com>
2023-03-15 01:42:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import {data} from './Data/MyData'
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+const [search,setSearch]=useState('');
+console.log(search);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+     <input type="search" onChange={(e)=>setSearch(e.target.value)}></input>
+ 
+
+      <table
+        border="1px solid black"
+        border-radius="10px"
+        border-collapse="collapse"
+      >
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {data.filter((item)=>{
+            return search.toLocaleUpperCase()===''
+            ?item:item.first_name.toLocaleLowerCase().includes(search);
+          }).map((item) => (
+            <tr key={item.id}>
+              <td>{item.first_name}</td>
+              <td>{item.last_name}</td>
+              <td>{item.email}</td>
+              <td>{item.Phone}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
